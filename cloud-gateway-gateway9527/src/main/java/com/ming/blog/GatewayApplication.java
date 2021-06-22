91404225058d35d675d546f2c205e4a597450d9f
MrJiangZM <Mr_JiangZM@163.com>
2021-06-22 21:51:29 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/ming/blog/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.ming.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author MrJiangZM
+ * @since <pre>2021/6/22</pre>
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
